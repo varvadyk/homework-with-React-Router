3a3971c156187ea72a3543ee36cc9bf6c08bcccd
varvadyk <diana_ivanivna@ukr.net>
2020-04-27 20:16:05 +0300

    change props and import icons

--- src/components/Posts/index.js
+++ src/components/Posts/index.js
@@ -1,32 +1,29 @@
 import React from "react";
 import '../../components/Posts/style.css';
-import { FaChevronCircleDown } from "react-icons/fa";
-import {FaChevronDown} from "react-icons/fa";
-import {FaRegComment}from "react-icons/fa";
-import {FiRepeat}from "react-icons/fi";
-import{FaRegHeart}from "react-icons/fa";
-import {FiUpload}from "react-icons/fi";
+import { FaChevronCircleDown,FaChevronDown,FaRegComment,FaRegHeart } from "react-icons/fa";
+import {FiRepeat,FiUpload}from "react-icons/fi";
 
 
-function Posts(props) {
-    return (
+const  Posts = ({photo,name,nickname,date,content,image} ) => {
+
+return (
         <div className="wrapper">
         <div  className="box">
           <div className="header_container">
-            <img className="photo_img" src={props.author.photo}></img>
+            <img className="photo_img" src={photo} alt="photo1"></img>
             <div className="header_flex">
             <div className="user_info">
-              <div className="author">{props.author.name}</div>
+              <div className="author">{name}</div>
             <div className="sub">  <FaChevronCircleDown  color="white"/></div>
-              <div className="sub">{props.author.nickname}</div>
+              <div className="sub">{nickname}</div>
               <div className="sub">•</div>
-              <div className=" sub ">{props.date}</div>
+              <div className=" sub ">{date}</div>
               <div className=" sub sub_down"><FaChevronDown /></div>
               </div>
-               <div className="content">{props.content}</div>
+               <div className="content">{content}</div>
               </div>
             </div>
-            <img className="starwarms_image" src={props.image}></img>
+            <img className="starwarms_image" src={image}  alt="photo1"></img>
           <div className="footer_icons">
   <div className ="comment"> <FaRegComment /><span className="number">482</span></div>
   <div className ="repeat"> <FiRepeat /><span className="number">146</span></div>
@@ -35,7 +32,6 @@ function Posts(props) {
           </div>
         </div>
       </div>
-        )
+        );
 }
-
 export default Posts;
