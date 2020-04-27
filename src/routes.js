7415a80551b98744f15674ff13ccaf80cfa24c7f
varvadyk <diana_ivanivna@ukr.net>
2020-04-27 20:22:08 +0300

    change location router

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import Home from './pages/Home';
 import Posts   from './pages/Posts';
 import Photos from './pages/Photos';
@@ -7,29 +6,31 @@ import Contacts from './pages/Contacts';
 
 
 export const routes=[
-   {
-      path:'/',
-      component: <Home />,
-      exact: true 
-   },
-   {
-    path:'/posts',
-    component: <Posts />
-
- },
-{
-
-    path:'/photos',
-    component: <Photos />
-
- },
+  
  
  {
     path:'/contacts',
     component: <Contacts />
 
  },
- 
+
+
+{
+
+   path:'/photos',
+   component: <Photos />
+
+},
+{
+   path:'/posts',
+   component: <Posts />
+
+},
+ {
+   path:'/',
+   component: <Home />,
+   exact: true 
+},
  {
     path:'*',
     component: ()=>(<div>page not found</div>),
